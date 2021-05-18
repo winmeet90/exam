b744dc88b643f84e15990bdb0b404b8ab7ec2351
winmeet90 <84111465+winmeet90@users.noreply.github.com>
2021-05-18 16:27:01 +0900

    (로또번호생성예제) v_1.5
    기존 코드를 클래스화

--- lotto.java
+++ lotto.java
@@ -1,26 +1,26 @@
-public class lotto {
-
-	public static void main(String[] args) {
-		//번호가 저장될 배열만들기
-		//번호생성(랜덤번호)
-		//중복 파악 후 저장
-		//출력
-		
+public class Lotto
+{
+	int[] marNumber = new int[6]; // 추상화
+	// 필요한 데이터 값
+	
+	Lotto3() // 생성자
+	{
+		//추가적인 코드는 여기에 작성.
+	}
+	// 로또 번호를 생성하는 메소드
+	boolean genLottoNumber()
+	{
 		int nNumber;
-		int[] arLotto = new int[6]; // 랜덤값이 저장될 배열
-		int pos = 0;
-		
 		boolean isExist = false;
-		
-		//2. 번호 생성
-		while(pos < arLotto.length)
+		int pos = 0;
+		while(pos < marNumber.length)
 		{
 			nNumber = (int)(Math.random() * 45) +1; // 1~45
 			
 			for(int i = 0; i < 6; i++)
 			{
 				//이미 그 번호가 존재함.
-				if(nNumber == arLotto[i])
+				if(nNumber == marNumber[i])
 				{
 					isExist = true;
 					break;
@@ -30,7 +30,7 @@ public class lotto {
 			if (isExist == false)
 			{
 				// 테이블에 저장
-				arLotto[pos] = nNumber;
+				marNumber[pos] = nNumber;
 				pos++;
 			}
 			else
@@ -38,10 +38,17 @@ public class lotto {
 				isExist = true;
 			}
 		}
-		for(int i = 0; i < arLotto.length; i++)
+		return true;
+	}
+	// 로또 번호를 복사하는 메소드
+	void getNumber(int[] arTarget)
+	{
+		for(int i=0; i<marNumber.length; i++)
 		{
-			System.out.println(arLotto[i]);
+			arTarget[i] = this.marNumber[i];
 		}
-	}	
-}		
+	}
+	
+}
+
 
